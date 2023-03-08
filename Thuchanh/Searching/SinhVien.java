46335357265a9c536fde7d4591ebbc57be63555c
nguyenson2003 <nqson123@gmail.com>
2023-03-08 12:42:42 +0700

    all

--- Thuchanh/Searching/SinhVien.java
+++ Thuchanh/Searching/SinhVien.java
@@ -21,7 +21,7 @@ public class SinhVien implements Comparable<SinhVien>
         this.Sdt=Sdt;
     }
     public SinhVien(String svStr){
-        StringTokenizer temp = new StringTokenizer(svStr);
+        StringTokenizer temp = new StringTokenizer(svStr,",");
         Ten = temp.nextToken();
         MaSV = Integer.parseInt(temp.nextToken());
         DiemTBC = Double.parseDouble(temp.nextToken());
@@ -42,40 +42,27 @@ public class SinhVien implements Comparable<SinhVien>
         return this.MaSV>b.MaSV?1:-1;
     }
     
-    
-    
-    public static class diemtbcOrder implements Comparator<SinhVien>{
-        @Override
-        public int compare(SinhVien o1, SinhVien o2) {
-            return o1.DiemTBC>o2.DiemTBC?1:-1;
-        }
+    public static  Comparator<SinhVien> diemtbcOrder(){
+        return (o1,o2)-> o1.DiemTBC>o2.DiemTBC?1:-1;
     }
 
-    public static class ngaysinhOrder implements Comparator<SinhVien>{
-        @Override
-        public int compare(SinhVien o1, SinhVien o2) {
-            return o1.NgaySinh.compareTo(o2.NgaySinh);
-        }
+    public static Comparator<SinhVien> ngaysinhOrder()  {
+        return (o1,o2)-> o1.NgaySinh.compareTo(o2.NgaySinh);
     }
 
-    public static class quequanOrder implements Comparator<SinhVien>{
-        @Override
-        public int compare(SinhVien o1, SinhVien o2) {
-            return o1.QueQuan.compareTo(o2.QueQuan);
-        }
+    public static  Comparator<SinhVien> quequanOrder(){
+        return (o1, o2) -> o1.QueQuan.compareTo(o2.QueQuan);
     }
 
-    public static class sdtOrder implements Comparator<SinhVien>{
-        @Override
-        public int compare(SinhVien o1, SinhVien o2) {
-            return o1.Sdt.compareTo(o2.Sdt);
-        }
+    public static Comparator<SinhVien> stdOrder() {
+        return (o1, o2) -> o1.Sdt.compareTo(o2.Sdt);
     }
-    
+
     @Override
     public String toString() {
         return String.format("%d - %10s - %.2f - %12s - %5s - %12s", MaSV, Ten, DiemTBC, NgaySinh, QueQuan, Sdt);
     }
+    
     
     public static void main(String []args) throws IOException {
         System.setIn(new FileInputStream(new File("sv.txt")));
@@ -99,5 +86,22 @@ public class SinhVien implements Comparable<SinhVien>
         for(String sdt:st2.keys()){
             StdOut.println(st2.get(sdt));
         }
+        StdOut.println();
+
+        //tìm dong huong
+        BST<String,Queue<SinhVien>> st3 = new BST<>();
+        for(long ma:st.keys()){
+            SinhVien val = st.get(ma);
+            if(!st3.contains(val.QueQuan())) st3.put(val.QueQuan(),new Queue<>());
+            st3.get(val.QueQuan()).enqueue(val);
+        }
+        for(String que:st3.keys()){
+            StdOut.println("Que "+que+":");
+            for(SinhVien val:st3.get(que)){
+                StdOut.println("\t"+val);
+            }
+        }
+        StdOut.println();
+        
     }
 }
